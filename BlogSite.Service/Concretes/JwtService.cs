e358bb220acf1e79514744479d62887af46aad79
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-10-29 19:15:39 +0300

    Update JwtService.cs
    JwtService  CreateToken Method uıpdate

--- BlogSite.Service/Concretes/JwtService.cs
+++ BlogSite.Service/Concretes/JwtService.cs
@@ -1,6 +1,9 @@
 ﻿using BlogSite.Models.Dtos.Tokens.Responses;
 using BlogSite.Models.Entities;
 using BlogSite.Service.Abstracts;
+using Core.Tokens.Config;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
@@ -8,12 +11,38 @@ namespace BlogSite.Service.Concretes;
 
 public sealed class JwtService : IJwtService
 {
-    public TokenResponseDto CreateJwtToken(User user)
+
+    private readonly TokenOption _tokenOption;
+
+    public JwtService(IOptions<TokenOption> tokenOption)
     {
-        throw new NotImplementedException();
+        _tokenOption = tokenOption.Value;
     }
 
-    public List<Claim> GetClaims(User user , List<string> audiences)
+    public TokenResponseDto CreateJwtToken(User user)
+    {
+        var accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOption.AccesTokenExpiration);
+        var secretKey = SecurityKeyHelper.GetSecurityKey(_tokenOption.SecurityKey);
+
+        SigningCredentials sc = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha512Signature);
+        JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(
+            issuer: _tokenOption.Issuer,
+            claims: GetClaims(user, _tokenOption.Audience),
+            expires: accessTokenExpiration,
+            signingCredentials: sc
+            );
+
+        var handler = new JwtSecurityTokenHandler();
+        string token = handler.WriteToken(jwtSecurityToken);
+
+        return new TokenResponseDto
+        {
+            AccessToken = token,
+            AccessTokenExpiration = accessTokenExpiration,
+        };
+    }
+
+    public List<Claim> GetClaims(User user, List<string> audiences)
     {
         var claims = new List<Claim>
         {
