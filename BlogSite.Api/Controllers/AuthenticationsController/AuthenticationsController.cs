a344de1408ad3025a526dfa6a7548cafe8720487
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-10-29 23:06:21 +0300

    AuthenticationService and Controller added.

---
+++ BlogSite.Api/Controllers/AuthenticationsController/AuthenticationsController.cs
@@ -0,0 +1,24 @@
+﻿using BlogSite.Models.Dtos.Users.Requests;
+using BlogSite.Service.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite.Api.Controllers.AuthenticationsController;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationsController(IAuthenticationService _authenticationService) : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginRequestDto loginRequestDto)
+    {
+        var result = await _authenticationService.LoginAsync(loginRequestDto);
+        return Ok(result);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterAsync([FromBody] RegisterRequestDto registerRequestDto)
+    {
+        var result = await _authenticationService.RegisterAsync(registerRequestDto);
+        return Ok(result);
+    }
+}
---
+++ BlogSite.Service/Concretes/AuthenticationService.cs
@@ -0,0 +1,37 @@
+﻿using BlogSite.Models.Dtos.Tokens.Responses;
+using BlogSite.Models.Dtos.Users.Requests;
+using BlogSite.Service.Abstracts;
+using Core.Responses;
+
+namespace BlogSite.Service.Concretes;
+
+public class AuthenticationService(IUserService1 _userService1, IJwtService _jwtService) : IAuthenticationService
+{
+    public async Task<ReturnModel<TokenResponseDto>> LoginAsync(LoginRequestDto dto)
+    {
+        var user = await _userService1.LoginAsync(dto);
+        var tokenResponse = _jwtService.CreateJwtToken(user);
+
+        return new ReturnModel<TokenResponseDto>
+        {
+            Data = tokenResponse,
+            Message = "Giriş Başarılı",
+            StatusCode = 200,
+            Success = true
+        };
+    }
+
+    public async Task<ReturnModel<TokenResponseDto>> RegisterAsync(RegisterRequestDto dto)
+    {
+        var user = await _userService1.RegisterAsync(dto);
+        var registerResponse = _jwtService.CreateJwtToken(user);
+
+        return new ReturnModel<TokenResponseDto>
+        {
+            Data = registerResponse,
+            Message = "Kayıt İşlemi Başarılı",
+            StatusCode = 200,
+            Success = true
+        };
+    }
+}
